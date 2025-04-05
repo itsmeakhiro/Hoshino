2f7c8ce1a0418f011535d59a88dcbe12bc80f3b5
Francis Loyd Raval <jasmarket577@gmail.com>
2025-04-05 20:58:21 +0800

    Update login.js

--- core/system/login.js
+++ core/system/login.js
@@ -1,11 +1,25 @@
-const login = require("fca-priyansh");
 const fs = require("fs-extra");
+const { execSync } = require("child_process");
+
+try {
+    require.resolve("fca-priyansh");
+} catch (e) {
+    console.log("fca-priyansh not found, installing...");
+    execSync("npm install fca-priyansh", { stdio: "inherit" });
+}
+
+const login = require("fca-priyansh");
 
 module.exports = async function login() {
-    login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf-8'))}, (err, api) => {
-        if(err) return console.error(err);
-    
+    login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, api) => {
+        if (err) {
+            return console.error('Login error:', err);
+        }
+        
         api.listen((err, message) => {
+            if (err) {
+                return console.error('Listener error:', err);
+            }
             api.sendMessage(message.body, message.threadID);
         });
     });
