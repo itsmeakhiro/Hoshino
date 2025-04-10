167f18dc921e55fc302d32351637707ef30411e1
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2025-04-10 03:56:30 +0000

    refactor: clean up formatting and improve error handling in aria command

--- Hoshino/modules/commands/aria.js
+++ Hoshino/modules/commands/aria.js
@@ -1,47 +1,50 @@
+const axios = require("axios").default;
+
 /**
  * @type {HoshinoLia.Command}
  */
-
-const axios = require("axios");
-
 const command = {
-    manifest: {
-     name: "aria",
-     aliases: ["aibrowser"],
-     version: "1.0.0",
-     developer: "Francis Loyd Raval",
-     description: "Meet Aria AI from Opera Browser developed by Kenneth Panio.",
-     category: "education",
-     cooldown: 0,
-     config: {
-        moderator: false,
-        admin: false,
-        privateOnly: false,
-     }
+  manifest: {
+    name: "aria",
+    aliases: ["aibrowser"],
+    version: "1.0.0",
+    developer: "Francis Loyd Raval",
+    description: "Meet Aria AI from Opera Browser developed by Kenneth Panio.",
+    category: "education",
+    cooldown: 0,
+    config: {
+      moderator: false,
+      admin: false,
+      privateOnly: false,
     },
-    style: {
-        type: "lines1",
-        title: "☄︎ ARIA AI",
-        footer: `Developed by: Francis Loyd Raval`,
-      },
-      font: {
-        title: "bold",
-        content: "sans",
-        footer: "sans",
-      },
-    async deploy({ chat, args, fonts }){
-        const q = args.join(" ");
-        if (!q){
-            return chat.reply(fonts.sans("Provide a query."));
-        }
-        try {
-         const aria = await axios.get(`https://haji-mix.up.railway.app/api/aria?ask=${encodeURIComponent(q)}&stream=false`);
-         const r = aria.data.answer;
-         chat.reply(r);
-        } catch (error) {
-            chat.reply(error.message)
-        }
+  },
+  style: {
+    type: "lines1",
+    title: "☄︎ ARIA AI",
+    footer: `Developed by: Francis Loyd Raval`,
+  },
+  font: {
+    title: "bold",
+    content: "sans",
+    footer: "sans",
+  },
+  async deploy({ chat, args, fonts }) {
+    const q = args.join(" ");
+    if (!q) {
+      return chat.reply(fonts.sans("Provide a query."));
     }
-}
+    try {
+      const aria = await axios.get(
+        `https://haji-mix.up.railway.app/api/aria?ask=${encodeURIComponent(
+          q
+        )}&stream=false`
+      );
+      const r = aria.data.answer;
+      chat.reply(r);
+    } catch (error) {
+      chat.reply(error instanceof Error ? String(error.stack) : String(error));
+    }
+  },
+};
 
 module.exports = command;
