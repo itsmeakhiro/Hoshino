ec78ad558c338849d72125ffe7d473dadd82d044
Francis Loyd Raval <jasmarket577@gmail.com>
2025-04-10 22:13:01 +0800

    ahh

--- core/system/handler/hoshinoAPI/hoshinoApi.js
+++ core/system/handler/hoshinoAPI/hoshinoApi.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const character = require("./plugins/character");
+const characters = require("./plugins/character");
 const education = require("./plugins/education");
 
 const app = express();
 
 module.exports = async function routers(){
-  app.use("/api", character);
+  app.use("/api", characters);
   app.use("/api", education);
 }
