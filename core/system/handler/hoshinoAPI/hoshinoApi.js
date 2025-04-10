c5246c5e14504a98d3f46ff3c0bbe3901d3fe60f
Francis Loyd Raval <jasmarket577@gmail.com>
2025-04-10 22:21:54 +0800

    fixed issue

--- core/system/handler/hoshinoAPI/hoshinoApi.js
+++ core/system/handler/hoshinoAPI/hoshinoApi.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const character = require("./plugins/characters");
-const education = require("./plugins/education");
 
 const app = express();
 
 module.exports = async function routers(){
   app.use("/api", character);
-  app.use("/api", education);
 }
