ba97dbb04303662227ada93b5dc7ddf0db8ecf27
Francis Loyd Raval <jasmarket577@gmail.com>
2025-04-12 12:29:19 +0800

    Create tate.js

---
+++ Hoshino/modules/commands/tate.js
@@ -0,0 +1,50 @@
+const axios = require("axios").default;
+
+/**
+ * @type {HoshinoLia.Command}
+ */
+const command = {
+  manifest: {
+    name: "tate",
+    aliases: ["topG"],
+    version: "1.0.0",
+    developer: "Francis Loyd Raval",
+    description: "Meet Andrew Tate or CobraTate, the top G AI bot ver developed by Francis Loyd Raval..",
+    category: "character",
+    cooldown: 0,
+    config: {
+      moderator: false,
+      admin: false,
+      privateOnly: false,
+    },
+  },
+  style: {
+    type: "lines1",
+    title: "💸 TATE AI",
+    footer: `Developed by: Francis Loyd Raval`,
+  },
+  font: {
+    title: "bold",
+    content: "sans",
+    footer: "sans",
+  },
+  async deploy({ chat, args, fonts }) {
+    const ask = args.join(" ");
+    if (!ask) {
+      return chat.reply(fonts.sans("Provide a query."));
+    }
+    try {
+      const { answer } = await chat.req(
+        "https://hoshino-14v4.onrender.com/tate",
+        {
+          ask,
+        }
+      );
+      chat.reply(answer);
+    } catch (error) {
+      chat.reply(error instanceof Error ? String(error.stack) : String(error));
+    }
+  },
+};
+
+module.exports = command;
